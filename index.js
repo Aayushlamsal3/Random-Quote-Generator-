8e03bb1865f2885b457ff8c8bd54ec390b50c2f0
Aayushlamsal3 <132969642+Aayushlamsal3@users.noreply.github.com>
2023-12-05 22:08:00 +0545

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+<div class="container">
+    <h1>Quote of the Day</h1>
+    <div class="quote">LOADING QUOTES...</div>
+    <div class="author">Aayush</div>
+    <button class="next" onclick="randomquotes(api_url)">NEXT QUOTE</button>
+</div>
